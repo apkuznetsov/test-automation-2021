4bf05e5f9fab21f666ef69a743fab5ffb2037ada
apkuznetsov <kouznts@ya.ru>
2021-09-27 07:41:18 +0400

    calculator UI

---
+++ 1-parameterized-and-dynamic-tests/src/main/java/lab/CalculatorUis.java
@@ -0,0 +1,21 @@
+package lab;
+
+public class CalculatorUis {
+    public static final String INPUT_A = "введите 1-ое число ... ";
+    public static final String INPUT_B = "введите 2-ое число ... ";
+    public static final String INCORRECT_INPUT = "ОШИБКА ВВОДА:\n" +
+            "введите число ... ";
+    public static final String TO_BINARY = "двоичное представление числа %s: %s";
+
+    public static final String CHOOSE_OPERATION = "введите операцию:\n" +
+            "доступны +, -, *, /, ^\n" +
+            "ввод ................. ";
+    public static final String INCORRECT_CHOICE = "ОШИБКА ВВОДА:\n" +
+            "введите операцию ..... ";
+    public static final String CHOSEN_OPERATION = "выбранная операция: %s";
+
+    public static final String OPERATION_RESULT = "%s\n%s\n%s\n=\n%s";
+    public static final String REPEAT = "продолжить";
+    public static final String REPEAT_OR_EXIT = "введите слово \"" + REPEAT + "\", чтобы начать сначала,\n" +
+            "или же любую другую строку, чтобы завершить программу:";
+}
