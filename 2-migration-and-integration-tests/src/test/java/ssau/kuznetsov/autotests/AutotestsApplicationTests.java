e25b6f743382d93b7328641b17d5c5d6d0e45d2b
apkuznetsov <kouznts@ya.ru>
2021-10-12 08:35:27 +0400

    create template

---
+++ 2-migration-and-integration-tests/src/test/java/ssau/kuznetsov/autotests/AutotestsApplicationTests.java
@@ -0,0 +1,13 @@
+package ssau.kuznetsov.autotests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutotestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
