fd1bb83bc0d1f3b72403a6fee2c69cc36db2a664
apkuznetsov <ethenaut@gmail.com>
2021-10-18 18:24:49 +0400

    def citizen model

---
+++ 2-migration-and-integration-tests/src/main/java/ssau/kuznetsov/autotests/model/Citizen.java
@@ -0,0 +1,81 @@
+package ssau.kuznetsov.autotests.model;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Table(name = "citizen")
+@Entity
+public class Citizen {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "surname", nullable = false)
+    private String surname;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "patronymic", nullable = false)
+    private String patronymic;
+    @Column(name = "birth_date", nullable = false)
+    private Date birthDate;
+    @Column(name = "birth_fed_place", nullable = false)
+    private String birthFedPlace;
+    @Column(name = "birth_place", nullable = false)
+    private String birthPlace;
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getBirthFedPlace() {
+        return birthFedPlace;
+    }
+
+    public void setBirthFedPlace(String birthFedPlace) {
+        this.birthFedPlace = birthFedPlace;
+    }
+
+    public String getBirthPlace() {
+        return birthPlace;
+    }
+
+    public void setBirthPlace(String birthPlace) {
+        this.birthPlace = birthPlace;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long code) {
+        this.id = code;
+    }
+}
