9b8e6dc9e98eb6741c3cd7fc6ecc32944bfbf520
apkuznetsov <ethenaut@gmail.com>
2021-10-19 08:53:07 +0400

    get passports by surname

--- 2-migration-and-integration-tests/src/main/java/ssau/kuznetsov/autotests/restcontroller/PassportRestcontroller.java
+++ 2-migration-and-integration-tests/src/main/java/ssau/kuznetsov/autotests/restcontroller/PassportRestcontroller.java
@@ -8,10 +8,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ssau.kuznetsov.autotests.dto.PassportResponse;
+import ssau.kuznetsov.autotests.model.Citizen;
 import ssau.kuznetsov.autotests.model.Passport;
+import ssau.kuznetsov.autotests.repository.CitizenRepository;
 import ssau.kuznetsov.autotests.repository.PassportRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/passport")
@@ -19,6 +22,8 @@ public class PassportRestcontroller {
 
     @Autowired
     private PassportRepository passportRepo;
+    @Autowired
+    private CitizenRepository citizenRepo;
 
     @GetMapping(path = "/{serialNumber}")
     public ResponseEntity passportBySerialNumber(
@@ -34,5 +39,25 @@ public class PassportRestcontroller {
         }
 
         return response;
+    }
+
+    @GetMapping(path = "/surname/{surname}")
+    public ResponseEntity passportsBySurname(
+            @PathVariable("surname") String surname) {
+
+        List<Citizen> citizens = citizenRepo.findAllBySurname(surname);
+        if (citizens == null || citizens.isEmpty()) {
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+        List<Long> citizenIds = citizens.stream().map(Citizen::getId).collect(Collectors.toList());
+
+        List<Passport> passports = passportRepo.findAllByCitizenIdIn(citizenIds);
+        if (passports == null || passports.isEmpty()) {
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        } else {
+            List<PassportResponse> passportResponses = passports.stream()
+                    .map(PassportResponse::new).collect(Collectors.toList());
+            return new ResponseEntity(passportResponses, HttpStatus.OK);
+        }
     }
 }
