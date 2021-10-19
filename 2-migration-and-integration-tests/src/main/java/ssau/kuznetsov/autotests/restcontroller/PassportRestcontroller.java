73d561f91819315844b8d3c23dc770169c043021
apkuznetsov <ethenaut@gmail.com>
2021-10-19 08:23:10 +0400

    refactor getting passport by serial number

--- 2-migration-and-integration-tests/src/main/java/ssau/kuznetsov/autotests/restcontroller/PassportRestcontroller.java
+++ 2-migration-and-integration-tests/src/main/java/ssau/kuznetsov/autotests/restcontroller/PassportRestcontroller.java
@@ -11,23 +11,28 @@ import ssau.kuznetsov.autotests.dto.PassportResponse;
 import ssau.kuznetsov.autotests.model.Passport;
 import ssau.kuznetsov.autotests.repository.PassportRepository;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/passport")
 public class PassportRestcontroller {
 
     @Autowired
     private PassportRepository passportRepo;
 
-    @GetMapping(path = "/passport/{serialNumber}")
+    @GetMapping(path = "/{serialNumber}")
     public ResponseEntity passportBySerialNumber(
             @PathVariable("serialNumber") long serialNumber) {
 
         Passport passport = passportRepo.findBySerialNumber(serialNumber);
 
+        ResponseEntity response;
         if (passport == null) {
-            return new ResponseEntity(HttpStatus.NO_CONTENT);
+            response = new ResponseEntity(HttpStatus.NO_CONTENT);
         } else {
-            return new ResponseEntity(new PassportResponse(passport), HttpStatus.OK);
+            response = new ResponseEntity(new PassportResponse(passport), HttpStatus.OK);
         }
+
+        return response;
     }
 }
