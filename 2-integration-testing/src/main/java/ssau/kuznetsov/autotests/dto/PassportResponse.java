b6654cb16745c497899fa9b95d6f3b594e1d68a4
Andrey <ethenaut@gmail.com>
2021-10-21 10:30:29 +0400

    Revert "refactor passport response"
    This reverts commit 638eec2bbee704eeb6e7efbdacc98f94c7d6edab.

--- 2-integration-testing/src/main/java/ssau/kuznetsov/autotests/dto/PassportResponse.java
+++ 2-integration-testing/src/main/java/ssau/kuznetsov/autotests/dto/PassportResponse.java
@@ -2,6 +2,7 @@ package ssau.kuznetsov.autotests.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import ssau.kuznetsov.autotests.model.Citizen;
+import ssau.kuznetsov.autotests.model.IssuingDivision;
 import ssau.kuznetsov.autotests.model.Passport;
 
 import java.io.Serializable;
@@ -9,57 +10,62 @@ import java.sql.Date;
 
 public class PassportResponse implements Serializable {
 
-    private Passport p;
+    private long serialNumber;
+    private String fullName;
+    private Date birthDate;
+    private String fullBirthPlace;
+    private String issuingDivisionName;
+    private long issuingDivisionCode;
+    private Date issueDate;
 
     public PassportResponse() {
     }
 
     public PassportResponse(Passport p) {
-        this.p = p;
+        Citizen c = p.getCitizen();
+        IssuingDivision i = p.getPassportIssuingDivision();
+
+        this.serialNumber = p.getSerialNumber();
+        this.fullName = c.getSurname() + " " + c.getName() + " " + c.getPatronymic();
+        this.birthDate = c.getBirthDate();
+        this.fullBirthPlace = c.getBirthFedPlace() + ", " + c.getBirthPlace();
+        this.issuingDivisionName = i.getName();
+        this.issuingDivisionCode = i.getCode();
+        this.issueDate = p.getIssueDate();
     }
 
     @JsonProperty("Серия и номер")
     public long getSerialNumber() {
-        return p.getSerialNumber();
+        return serialNumber;
     }
 
     @JsonProperty("ФИО")
     public String getFullName() {
-        Citizen c = p.getCitizen();
-        return c.getSurname() + " " + c.getName() + " " + c.getPatronymic();
+        return fullName;
     }
 
     @JsonProperty("Дата рождения")
     public Date getBirthDate() {
-        return p.getCitizen().getBirthDate();
+        return birthDate;
     }
 
     @JsonProperty("Место рождения")
     public String getFullBirthPlace() {
-        Citizen c = p.getCitizen();
-        return c.getBirthFedPlace() + ", " + c.getBirthPlace();
+        return fullBirthPlace;
     }
 
     @JsonProperty("Паспорт выдан")
     public String getIssuingDivisionName() {
-        return p.getPassportIssuingDivision().getName();
+        return issuingDivisionName;
     }
 
     @JsonProperty("Код подразделения")
     public long getIssuingDivisionCode() {
-        return p.getPassportIssuingDivision().getCode();
+        return issuingDivisionCode;
     }
 
     @JsonProperty("Дата выдачи")
     public Date getIssueDate() {
-        return p.getIssueDate();
-    }
-
-    public String getSurname() {
-        return p.getCitizen().getSurname();
-    }
-
-    public String getName() {
-        return p.getCitizen().getName();
+        return issueDate;
     }
 }
