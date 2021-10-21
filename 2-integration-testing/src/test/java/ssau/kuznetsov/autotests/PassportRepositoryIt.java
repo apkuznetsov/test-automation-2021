3de1bc63f2a3f7dbf77e02c21725d140acc1d54b
Andrey <ethenaut@gmail.com>
2021-10-21 11:48:33 +0400

    get correct passport by surname name birthdate

--- 2-integration-testing/src/test/java/ssau/kuznetsov/autotests/PassportRepositoryIt.java
+++ 2-integration-testing/src/test/java/ssau/kuznetsov/autotests/PassportRepositoryIt.java
@@ -7,6 +7,7 @@ import org.springframework.http.*;
 import ssau.kuznetsov.autotests.dto.PassportResponse;
 
 import java.sql.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -16,6 +17,10 @@ public class PassportRepositoryIt extends BaseIt {
     private static final String apiPath = "/api/passport/";
 
     private static final long expectedSerialNumber = 1678756113;
+    private static final String expectedFullName = "Морозов Артём Тимофеевич";
+    private static final String expectedSurname = "Морозов";
+    private static final String expectedName = "Артём";
+    private static final Date expectedBirthDate = Date.valueOf("1957-10-08");
     private static final Date expectedIssueDate = Date.valueOf("2012-05-17");
     private static final long noSuchSerialNumber = -1;
 
@@ -54,5 +59,24 @@ public class PassportRepositoryIt extends BaseIt {
         // assert
         assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
         assertNull(result);
+    }
+
+    @Test
+    @FlywayTest
+    public void get_correct_passport_by_surname_name_birthdate() {
+        // arrange
+        String testUrl = apiPath + expectedSurname + "/" + expectedName + "/" + expectedBirthDate.toString();
+        final HttpEntity<String> request = new HttpEntity<>(null, new HttpHeaders());
+
+        // act
+        final ResponseEntity<List<PassportResponse>> response = restTemplate.exchange(
+                testUrl, HttpMethod.GET, request, new ParameterizedTypeReference<>() {
+                });
+        PassportResponse result = response.getBody().get(0);
+
+        // assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(expectedFullName, result.getFullName());
+        assertEquals(expectedIssueDate.toString(), result.getIssueDate().toString());
     }
 }
