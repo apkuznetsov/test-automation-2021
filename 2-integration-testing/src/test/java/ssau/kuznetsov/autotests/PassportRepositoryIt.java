22325f445c8b4173bd0046cb039329718d044f5a
Andrey <ethenaut@gmail.com>
2021-10-21 09:35:18 +0400

    no content when get passport by no such serial number

--- 2-integration-testing/src/test/java/ssau/kuznetsov/autotests/PassportRepositoryIt.java
+++ 2-integration-testing/src/test/java/ssau/kuznetsov/autotests/PassportRepositoryIt.java
@@ -9,6 +9,7 @@ import ssau.kuznetsov.autotests.dto.PassportResponse;
 import java.sql.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class PassportRepositoryIt extends BaseIt {
 
@@ -16,6 +17,7 @@ public class PassportRepositoryIt extends BaseIt {
 
     private static final long expectedSerialNumber = 1678756113;
     private static final Date expectedIssueDate = Date.valueOf("2012-05-17");
+    private static final long noSuchSerialNumber = -1;
 
     @Test
     @FlywayTest
@@ -34,5 +36,23 @@ public class PassportRepositoryIt extends BaseIt {
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals(expectedSerialNumber, result.getSerialNumber());
         assertEquals(expectedIssueDate.toString(), result.getIssueDate().toString());
+    }
+
+    @Test
+    @FlywayTest
+    public void no_content_when_get_passport_by_no_such_serial_number() {
+        // arrange
+        String testUrl = apiPath + noSuchSerialNumber;
+        final HttpEntity<String> request = new HttpEntity<>(null, new HttpHeaders());
+
+        // act
+        final ResponseEntity<PassportResponse> response = restTemplate.exchange(
+                testUrl, HttpMethod.GET, request, new ParameterizedTypeReference<>() {
+                });
+        PassportResponse result = response.getBody();
+
+        // assert
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+        assertNull(result);
     }
 }
