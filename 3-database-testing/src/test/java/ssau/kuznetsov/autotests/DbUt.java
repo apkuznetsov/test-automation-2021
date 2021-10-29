16e180cb1323a0fcb46a26cf82271fb99a8fb54a
Andrey <ethenaut@gmail.com>
2021-10-29 22:14:31 +0400

    test citizen dataset

--- 3-database-testing/src/test/java/ssau/kuznetsov/autotests/DbUt.java
+++ 3-database-testing/src/test/java/ssau/kuznetsov/autotests/DbUt.java
@@ -4,8 +4,11 @@ import org.dbunit.Assertion;
 import org.dbunit.IDatabaseTester;
 import org.dbunit.JdbcDatabaseTester;
 import org.dbunit.dataset.ITable;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.flywaydb.test.annotation.FlywayTest;
 import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
 
 public class DbUt extends PostgresqlContainer {
 
@@ -22,9 +25,10 @@ public class DbUt extends PostgresqlContainer {
 
     @Test
     @FlywayTest
-    public void sample() throws Exception {
-        ITable expectedTable = tester.getConnection().createDataSet().getTable("passport");
-        ITable actualTable = tester.getConnection().createDataSet().getTable("passport");
+    public void is_citizen_table_there() throws Exception {
+        ITable expectedTable = new FlatXmlDataSetBuilder().build(
+                new FileInputStream("dataset-citizen.xml")).getTable("citizen");
+        ITable actualTable = tester.getConnection().createDataSet().getTable("citizen");
 
         Assertion.assertEquals(expectedTable, actualTable);
     }
