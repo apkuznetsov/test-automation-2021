cbc8f6076b7e88761947f1adb58ed9a07b9064b2
Andrey <ethenaut@gmail.com>
2021-10-30 19:57:20 +0400

    reformat init DB script

--- 3-database-testing/src/main/resources/db/migration/V1__Init_DB.sql
+++ 3-database-testing/src/main/resources/db/migration/V1__Init_DB.sql
@@ -1,35 +1,39 @@
-create table citizen (
-    id bigserial not null,
-    birth_date date not null,
+create table citizen
+(
+    id              bigserial    not null,
+    birth_date      date         not null,
     birth_fed_place varchar(255) not null,
-    birth_place varchar(255) not null,
-    name varchar(255) not null,
-    patronymic varchar(255) not null,
-    surname varchar(255) not null,
+    birth_place     varchar(255) not null,
+    name            varchar(255) not null,
+    patronymic      varchar(255) not null,
+    surname         varchar(255) not null,
     primary key (id)
 );
 
-create table foreign_passport (
-    serial_number bigserial not null,
-    issue_date date not null,
-    validity_date date not null,
-    citizen_id bigint not null,
+create table foreign_passport
+(
+    serial_number         bigserial not null,
+    issue_date            date      not null,
+    validity_date         date      not null,
+    citizen_id            bigint    not null,
     issuing_division_code bigint,
     primary key (serial_number)
 );
 
-create table issuing_division (
-    code bigserial not null,
+create table issuing_division
+(
+    code bigserial    not null,
     name varchar(255) not null,
     primary key (code)
- );
+);
 
-create table passport (
-    serial_number bigserial not null,
-    issue_date date not null,
-    validity_date date not null,
-    citizen_id bigint not null,
-    issuing_division_code bigint not null,
+create table passport
+(
+    serial_number         bigserial not null,
+    issue_date            date      not null,
+    validity_date         date      not null,
+    citizen_id            bigint    not null,
+    issuing_division_code bigint    not null,
     primary key (serial_number)
 );
 
