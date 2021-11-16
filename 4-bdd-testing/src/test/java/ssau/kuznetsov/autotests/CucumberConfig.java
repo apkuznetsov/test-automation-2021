77d02d47fb45c21abb4d23f7730eaa4e3aaa1107
Andrey <ethenaut@gmail.com>
2021-11-16 17:44:22 +0400

    Cucumber config

---
+++ 4-bdd-testing/src/test/java/ssau/kuznetsov/autotests/CucumberConfig.java
@@ -0,0 +1,23 @@
+package ssau.kuznetsov.autotests;
+
+import io.cucumber.java.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootContextLoader;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ContextConfiguration;
+
+// class to use Spring app context while running Cucumber
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@ContextConfiguration(classes = App.class, loader = SpringBootContextLoader.class)
+public class CucumberConfig {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CucumberConfig.class);
+
+    // need this method so Cucumber will recognize this class as glue and load Spring context config
+    @Before
+    public void setUp() {
+        LOG.info("SPRING CONTEXT INITED FOR EXECUTING CUCUMBER TESTS");
+    }
+}
