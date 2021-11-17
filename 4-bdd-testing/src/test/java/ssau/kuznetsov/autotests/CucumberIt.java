4ddd87fb8773befcc6cba72febeae14141e5e790
Andrey <ethenaut@gmail.com>
2021-11-17 18:47:07 +0400

    fix IT options

--- 4-bdd-testing/src/test/java/ssau/kuznetsov/autotests/CucumberIt.java
+++ 4-bdd-testing/src/test/java/ssau/kuznetsov/autotests/CucumberIt.java
@@ -4,8 +4,5 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features",
-        plugin = {"pretty", "html:target/cucumber"})
 public class CucumberIt {
 }
