639e5ad7f5d54d2338f35786d834af8f59545882
Andrey <ethenaut@gmail.com>
2021-11-19 00:12:24 +0400

    fix create test passport with such serial number

--- 4-bdd-testing/src/main/java/ssau/kuznetsov/autotests/controllers/AdminController.java
+++ 4-bdd-testing/src/main/java/ssau/kuznetsov/autotests/controllers/AdminController.java
@@ -42,16 +42,17 @@ public class AdminController {
 
     @GetMapping("create/passport/{serialNumber}")
     public void createTestPassportWithSuchSerialNumber(@PathVariable long serialNumber) {
-        Citizen cit = new Citizen(26L, "Морозов", "Артём", "Тимофеевич",
+        Citizen cit = new Citizen(99L, "Морозов", "Артём", "Тимофеевич",
                 "1957-10-08", "Республика Северная Осетия — Алания", "Гусь-Хрустальный");
-        IssuingDivision div = new IssuingDivision(100001L,
+        IssuingDivision div = new IssuingDivision(999_999L,
                 "Отдел по вопросам миграции ОП №4 (Октябрьский район) УМВД России по г. Самаре");
-        Passport pass = new Passport(1678756113L, "2012-05-17", "2031-09-10");
+        Passport pass = new Passport(serialNumber, "2012-05-17", "2031-09-10");
+
+        cit = citRep.saveAndFlush(cit);
+        div = divRep.saveAndFlush(div);
+
         pass.setCitizen(cit);
         pass.setPassportIssuingDivision(div);
-
-        citRep.save(cit);
-        divRep.save(div);
         passRep.save(pass);
     }
 }
