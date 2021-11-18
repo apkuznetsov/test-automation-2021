16a09014b301ff21fec065d515f84e7d3df5f931
Andrey <ethenaut@gmail.com>
2021-11-18 14:13:33 +0400

    use HTTP clients

--- 4-bdd-testing/src/test/java/ssau/kuznetsov/autotests/BddIt.java
+++ 4-bdd-testing/src/test/java/ssau/kuznetsov/autotests/BddIt.java
@@ -1,45 +1,39 @@
 package ssau.kuznetsov.autotests;
 
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.*;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpStatus;
 import ssau.kuznetsov.autotests.dtos.PassportResponse;
+import ssau.kuznetsov.autotests.helpers.AdminHttpClient;
+import ssau.kuznetsov.autotests.helpers.UserHttpClient;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BddIt {
 
-    private final int port = 8080;
-    private final String url = "http://localhost:" + port + "/api/passport/";
+    private final AdminHttpClient admin = new AdminHttpClient();
+    private final UserHttpClient user = new UserHttpClient();
 
-    private final RestTemplate restTemplate = new RestTemplate();
-    private ResponseEntity<PassportResponse> latestResponse;
-
-    public void get(long serialNumber) {
-        latestResponse = restTemplate.exchange(
-                url + serialNumber,
-                HttpMethod.GET,
-                new HttpEntity<>(null, new HttpHeaders()),
-                new ParameterizedTypeReference<>() {
-                });
+    @Before
+    public void resetDatabase() {
+        admin.truncatePassport();
     }
 
     @When("^клиент посылает запрос GET /api/passport/ (\\d+)$")
     public void client_gets_passport_by_serial_number(long serialNumber) {
-        get(serialNumber);
+        user.get(serialNumber);
     }
 
     @Then("^получает код состояния (\\d+)$")
     public void client_receives_status_code_of(int expectedStatusCode) {
-        HttpStatus actualStatusCode = latestResponse.getStatusCode();
+        HttpStatus actualStatusCode = user.latestResponse().getStatusCode();
         assertEquals(HttpStatus.OK, actualStatusCode);
     }
 
     @Then("^получает паспорт с серийным номером (\\d+)$")
     public void the_client_receives_status_code_of(long expectedSerialNumber) {
-        PassportResponse result = latestResponse.getBody();
+        PassportResponse result = user.latestResponse().getBody();
         assertEquals(expectedSerialNumber, result.getSerialNumber());
     }
 }
