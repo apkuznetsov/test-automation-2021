96daf83c5836068b3fbacd53981795bf52fa8a9b
Andrey <ethenaut@gmail.com>
2021-11-19 11:35:44 +0400

    def second feature steps

--- 4-bdd-testing/src/test/java/ssau/kuznetsov/autotests/BddIt.java
+++ 4-bdd-testing/src/test/java/ssau/kuznetsov/autotests/BddIt.java
@@ -5,11 +5,14 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.springframework.http.HttpStatus;
-import ssau.kuznetsov.autotests.dtos.PassportResponse;
 import ssau.kuznetsov.autotests.helpers.AdminHttpClient;
 import ssau.kuznetsov.autotests.helpers.UserHttpClient;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BddIt {
 
@@ -26,9 +29,19 @@ public class BddIt {
         admin.createTestPassportWithSuchSerialNumber(serialNumber);
     }
 
+    @Given("^в базе данных есть паспорты с фамилией и именем (.+) (.+)$")
+    public void there_is_passports_with_such_surname_and_name(String surname, String name) {
+        admin.createTestPassportsWithSuchSurnameAndName(surname, name);
+    }
+
     @When("^пользователь посылает запрос GET /api/passport/ (\\d+)$")
     public void user_gets_passport_by_serial_number(long serialNumber) {
         user.get(serialNumber);
+    }
+
+    @When("^пользователь посылает запрос GET /api/passport/surname/ (.+) /name/ (.+)$")
+    public void user_gets_passports_by_surname_and_name(String surname, String name) {
+        user.getPassportsBySurnameAndName(surname, name);
     }
 
     @Then("^получает код состояния (\\d+)$")
@@ -43,5 +56,16 @@ public class BddIt {
         System.out.println(passportResponse);
 
         assertEquals(expectedSerialNumber, passportResponse.get("Серия и номер"));
+    }
+
+    @Then("^получает список паспортов с фамилией и именем (.+) (.+)$")
+    public void user_receives_passport_with_such_surname_and_name(String expectedSurname, String expectedName) {
+        ArrayList<LinkedHashMap> passportResponseList = (ArrayList<LinkedHashMap>) user.latestResponse().getBody();
+        System.out.println(passportResponseList);
+
+        String expectedSurnameAndName = expectedSurname + " " + expectedName;
+        for (LinkedHashMap p : passportResponseList) {
+            assertTrue(p.get("ФИО").toString().startsWith(expectedSurnameAndName));
+        }
     }
 }
