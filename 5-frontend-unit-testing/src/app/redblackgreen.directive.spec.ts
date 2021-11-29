ef26035e3b3ef5c07fac2baf1caf1fc60aaa7e98
Andrey <ethenaut@gmail.com>
2021-11-29 17:48:04 +0400

    impl highlight directive

---
+++ 5-frontend-unit-testing/src/app/redblackgreen.directive.spec.ts
@@ -0,0 +1,9 @@
+import {RedblackgreenDirective} from './redblackgreen.directive';
+
+describe('RedblackgreenDirective', () => {
+  it('should create an instance', () => {
+    // @ts-ignore
+    const directive = new RedblackgreenDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ 5-frontend-unit-testing/src/app/redblackgreen.directive.ts
@@ -0,0 +1,21 @@
+import {Directive, ElementRef, HostListener} from '@angular/core';
+
+@Directive({
+  selector: '[appRedblackgreen]'
+})
+export class RedblackgreenDirective {
+  constructor(private el: ElementRef) {
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('yellow');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight('');
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+}
