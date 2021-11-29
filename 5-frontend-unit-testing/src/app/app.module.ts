f85bff88a2a051057c0268da506df6c6a61fa9a0
Andrey <ethenaut@gmail.com>
2021-11-29 16:56:35 +0400

    impl inputs component

--- 5-frontend-unit-testing/src/app/app.module.ts
+++ 5-frontend-unit-testing/src/app/app.module.ts
@@ -4,10 +4,11 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {routing} from './app.route';
 import {FormsModule} from "@angular/forms";
+import {InputsComponent} from "./inputs.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, InputsComponent
   ],
   imports: [
     BrowserModule,
