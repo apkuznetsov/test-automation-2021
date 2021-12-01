5502a9d3e8dbbac6ad6a110405bb52ec323e3241
Andrey <ethenaut@gmail.com>
2021-12-01 17:28:40 +0400

    use rounding pipe

--- 5-frontend-unit-testing/src/app/app.module.ts
+++ 5-frontend-unit-testing/src/app/app.module.ts
@@ -6,10 +6,11 @@ import {routing} from './app.route';
 import {FormsModule} from "@angular/forms";
 import {InputsComponent} from "./inputs.component";
 import {RedblackgreenDirective} from './redblackgreen.directive';
+import {RoundingPipe} from "./rounding.pipe";
 
 @NgModule({
   declarations: [
-    AppComponent, InputsComponent, RedblackgreenDirective
+    AppComponent, InputsComponent, RedblackgreenDirective, RoundingPipe
   ],
   imports: [
     BrowserModule,
